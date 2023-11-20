5dca8679bd5cfc7ce2fd07163c8badd5c563a6e8
Gustavo Peruzzo <gustavoperuzzo1311@hotmail.com>
2023-11-20 16:06:02 -0300

    modified:   src/components/footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,10 +1,11 @@
 import logo from '@/assets/logo-branca.png'
 import { Facebook, Instagram, Twitter } from 'lucide-react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Footer() {
   return (
-    <footer className='bg-[#242424] text-white w-full h-[250px] px-6 py-12 md:p-12'>
+    <footer className='bg-[#242424] text-white w-full px-6 py-12 md:p-12'>
       <div className='w-full flex justify-between items-center border-b-2 border-white pb-8'>
         <div>
           <Image
@@ -21,7 +22,9 @@ export default function Footer() {
         <div className='flex gap-6 md:gap-8'>
           <Facebook className='md:w-8 md:h-8' />
 
-          <Instagram className='md:w-8 md:h-8' />
+          <Link href={'https://www.instagram.com/conexaoculturalcvel/'}>
+            <Instagram className='md:w-8 md:h-8' />
+          </Link>
 
           <Twitter className='md:w-8 md:h-8' />
         </div>
