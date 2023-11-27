268cacc5b78ba53e81b5afe6ec8a2e1affe4adfc
Gustavo Peruzzo <gustavoperuzzo1311@hotmail.com>
2023-11-27 11:03:29 -0300

    Fix bug in login functionality

--- src/app/api/events/[eventId]/route.ts
+++ src/app/api/events/[eventId]/route.ts
@@ -16,7 +16,7 @@ export async function PATCH(
 
     const event = await db.event.update({
       where: { id: params.eventId },
-      data: { highlight, published: true },
+      data: { highlight: highlight, published: true },
     })
 
     return NextResponse.json(event)
